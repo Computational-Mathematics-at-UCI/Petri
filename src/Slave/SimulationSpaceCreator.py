9e8cb6ede92c26c69c1b9d5d355d7dbae8a4fdd1
Leo <leo.ram.cayon@gmail.com>
2021-10-24 13:52:51 -0400

    AAdicionando read y write en IO

--- src/Slave/SimulationSpaceCreator.py
+++ src/Slave/SimulationSpaceCreator.py
@@ -2,6 +2,6 @@
 
 class SimulationSpaceCreator:
 
-    def __init__(self, molecules, number_of_threads=1):
-        self.molecules = molecules
+    def __init__(self, molecules_address, number_of_threads=1):
+        self.molecules_address = molecules_address
         self.number_of_threads = number_of_threads
--- src/Slave/Worker.py
+++ src/Slave/Worker.py
@@ -1,5 +1,6 @@
 import threading
 
+
 class Worker(threading.Thread):
 
     def __init__(self):
